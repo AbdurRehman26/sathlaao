78389d84d6bc8d8c79ebeb9b62dc92084fd97ff9
s.kazmi <s.kazmi@metro-markets.de>
2025-06-02 22:41:43 +0200

    fixed

---
+++ app/Filament/Pages/Auth/Register.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+use App\Models\User;
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
+use Filament\Pages\Auth\Register as BaseRegister;
+
+class Register extends BaseRegister
+{
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getNameFormComponent(),
+                        $this->getEmailFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getPasswordConfirmationFormComponent(),
+                        $this->getPhoneFormComponent(),
+                        $this->getFacebookProfileFormComponent(),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
+    }
+
+    protected function getPhoneFormComponent(): Component
+    {
+        return TextInput::make('phone_number')->label('Phone Number (Optional)')
+            ->tel()
+            ->maxLength(15)
+            ->placeholder('+4695XXXXXXXX')
+            ->required(false);
+    }
+
+    protected function getFacebookProfileFormComponent(): Component
+    {
+        return TextInput::make('facebook_profile')->label('Facebook Profile Url (Optional)');
+    }
+
+    protected function handleRegistration(array $data): \Illuminate\Database\Eloquent\Model
+    {
+        /** @var User $user */
+        $user = parent::handleRegistration($data);
+
+        $user->sendEmailVerificationNotification();
+
+        return $user;
+    }
+}
