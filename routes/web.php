a1fe60c072bf8d0917d2d6f5c994d71461afce18
s.kazmi <s.kazmi@metro-markets.de>
2025-06-06 02:00:53 +0200

    verify email issue resolved

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('/app');
 });
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/app');
+})->middleware(['auth', 'signed'])->name('verification.verify');
